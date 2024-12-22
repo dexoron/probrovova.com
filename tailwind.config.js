b7da8ee2bc45385d65bd69bdf9f97eca3d290222
PROBROVOVAyt <probrovova1@gmail.com>
2024-12-22 18:38:00 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./index.html", "./pages/**/*.html"],
   theme: {
-    screens: {
-      mobile: "360px",
-      tablet: "560px",
-      tablet2: "820px",
-      desktop: "1024px"
-    },
     extend: {}
   },
   plugins: []
